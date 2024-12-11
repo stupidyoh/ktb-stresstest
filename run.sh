56bca7505861d653558bafbd259f1fd72b08c687
jerry.kim <151607657+jerrydotkim@users.noreply.github.com>
2024-12-11 15:27:56 +0900

    Add tag change code

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 사용법 및 스크립트 설명
-# 이 스크립트는 지정된 YAML 파일에서 flowFunction, duration, arrivalRate 값을 원하는 값으로 변경합니다.
-# 사용법: ./update_scenario.sh <새로운 flowFunction 값> <duration 값> <arrivalRate 값>
+# 이 스크립트는 지정된 YAML 파일에서 flowFunction, duration, arrivalRate, target 값을 원하는 값으로 변경합니다.
+# 사용법: ./update_scenario.sh <새로운 flowFunction 값> <duration 값> <arrivalRate 값> <새로운 target 값>
 # 사용 가능한 시나리오 function:
 # - registerUser
 # - loginUser
@@ -17,11 +17,12 @@
 NEW_FLOW_FUNCTION="$1"
 DURATION=$2
 ARRIVALRATE=$3
+NEW_TARGET="$4"
 TARGET_FILE="playwright-artillery.yml"
 
-if [ -z "$NEW_FLOW_FUNCTION" ] || [ -z "$DURATION" ] || [ -z "$ARRIVALRATE" ]; then
-  echo "사용법: $0 <새로운 flowFunction 값> <duration 값> <arrivalRate 값>"
-  echo "예시: $0 loginUser 5 10"
+if [ -z "$NEW_FLOW_FUNCTION" ] || [ -z "$DURATION" ] || [ -z "$ARRIVALRATE" ] || [ -z "$NEW_TARGET" ]; then
+  echo "사용법: $0 <새로운 flowFunction 값> <duration 값> <arrivalRate 값> <새로운 target 값>"
+  echo "예시: $0 loginUser 5 10 https://new-domain.example.com"
   exit 1
 fi
 
@@ -34,11 +35,18 @@ fi
 # flowFunction 값 변경
 sed -i.bak "s/flowFunction: \"[^\"]*\"/flowFunction: \"$NEW_FLOW_FUNCTION\"/" "$TARGET_FILE"
 
-sed -i.bak -E "s/^[[:space:]]*-[[:space:]]*duration: [0-9]+/  - duration: $DURATION/" $TARGET_FILE
-sed -i.bak -E "s/^[[:space:]]*arrivalRate: [0-9]+/    arrivalRate: $ARRIVALRATE/" $TARGET_FILE
+# duration 값 변경
+sed -i.bak -E "s/^[[:space:]]*-[[:space:]]*duration: [0-9]+/  - duration: $DURATION/" "$TARGET_FILE"
 
-echo "flowFunction 값이 \"$NEW_FLOW_FUNCTION\"로, duration이 \"$DURATION\"으로, arrivalRate가 \"$ARRIVALRATE\"로 성공적으로 변경되었습니다!"
+# arrivalRate 값 변경
+sed -i.bak -E "s/^[[:space:]]*arrivalRate: [0-9]+/    arrivalRate: $ARRIVALRATE/" "$TARGET_FILE"
+
+# target 값 변경 (target 문자열 기반)
+sed -i.bak "s|target: .*|target: $NEW_TARGET|" "$TARGET_FILE"
+
+echo "flowFunction 값이 \"$NEW_FLOW_FUNCTION\"로, duration이 \"$DURATION\"으로, arrivalRate가 \"$ARRIVALRATE\"로, target이 \"$NEW_TARGET\"로 성공적으로 변경되었습니다!"
 echo "대상 파일: $TARGET_FILE"
 
 # 실행
-artillery run $TARGET_FILE
+artillery run "$TARGET_FILE"
+
