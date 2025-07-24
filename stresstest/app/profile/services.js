1b94f89b4acffb114a3eab9811713dc30ec56a78
grey-leeeee <grey.lee@goorm.io>
2025-07-24 11:51:34 +0900

    fix : 프로필 이미지 변경 시 camera 버튼 찾도록 수정

--- stresstest/app/profile/services.js
+++ stresstest/app/profile/services.js
@@ -4,16 +4,20 @@ const path = require('path');
 const addProfileImage = async (page, filename) => {
   await page.locator('//*[@id="__next"]/div/nav/div/div/div[3]/div/button').click();
   await page.locator('//*[@id="__next"]/div/nav/div/div/div[3]/div/div/button[1]').click();
+
+  await page.waitForTimeout(3000);
+  const cameraButton = await page.locator('button[class*="vapor-Button-md"]');
+
   console.log(page.url());
   const [fileChooser] = await Promise.all([
-      page.waitForEvent('filechooser'),
-      page.click('//*[@id="radix-:rj:"]/div/div/div/div[2]/button'),
+    page.waitForEvent('filechooser'),
+    cameraButton.click(),
   ]);
-  
+
   await fileChooser.setFiles(path.resolve(filename));
   await page.getByRole('button', { name: '저장' }).click();
   await page.waitForTimeout(3000);
-  
+
   console.info('Profile image added');
 };
 
